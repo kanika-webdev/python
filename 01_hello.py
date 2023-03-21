b6b070d82946038c997e12d3071cc029d89bf8a1
Kanika Tyagi <110611227+kanika-webdev@users.noreply.github.com>
2023-03-21 19:47:59 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+# triple cote is used to write the paragraph
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
---
+++ 02_playsound.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+playsound("C:\\Users\\kanik\\Desktop\\python_course\\Bella-Ciao-Money-Heist.mp3")
+# playsound('C:\Users\kanik\Desktop\python_course\Bella-Ciao-Money-Heist.mp3')
